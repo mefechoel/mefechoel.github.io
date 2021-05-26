8d40b0765ab7d7bca3eab910ddc46924ca82c4e4
mefechoel <michel.strelow97@gmail.com>
2021-05-26 21:59:52 +0200

    delete

--- assets/Pixelnetz.7456f864.js
+++
@@ -1,2 +0,0 @@
-/*! Licenses of used libraries, fonts and other software can be found at /lib-licenses.txt */
-import{a as s}from"./vendor.a325d5f6.js";var a="s37qM0",d="s9JGMr",e="s2Y124",m="slXpp2";const v=()=>s("div",{className:a},s("div",{className:d},s("div",{className:e},s("div",{className:m})),s("div",{className:e},s("div",{className:m})),s("div",{className:e},s("div",{className:m}))));export{v as P};
--- assets/TextContainer.26dd32e7.js
+++
@@ -1,2 +0,0 @@
-/*! Licenses of used libraries, fonts and other software can be found at /lib-licenses.txt */
-import{a as s,c as a}from"./vendor.a325d5f6.js";var r="s1x4OC";const c=({children:c,className:e})=>s("div",{className:a(r,e)},c);export{c as T};
